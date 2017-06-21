373f68aa849526932e6554f24f453f56cce46af0
Daniel Kostro <kostro.d@gmail.com>
2017-06-21 18:11:08 +0200

    fix config: don't use const in view

--- config.js
+++ config.js
@@ -433,9 +433,11 @@ module.exports = {
             nmrIndex: {
                 map: function(doc) {
                     var nmr = require('views/lib/nmr');
-                    const nmrIndexes = nmr.getIndexes(doc);
-                    for(var i=0; i<nmrIndexes.length; i++) {
-                        emitWithOwner(null, nmrIndexes[i])
+                    var nmrIndexes = nmr.getIndexes(doc);
+                    if(nmrIndexes) {
+                        for(var i=0; i<nmrIndexes.length; i++) {
+                            emitWithOwner(null, nmrIndexes[i])
+                        }
                     }
                 },
                 designDoc: 'nmr',
