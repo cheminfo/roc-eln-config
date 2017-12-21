c4d6dc87de0c8b5225180aafdc396a1c1d1c7972
Luc Patiny <luc@patiny.com>
2017-12-21 14:14:39 +0100

    add nb 1h and 13c

--- getToc.js
+++ getToc.js
@@ -6,13 +6,23 @@ exports.getToc = function(doc) {
     var nmr = spectra.nmr;
     var ir = spectra.ir;
     var mass = spectra.mass;
-    var nb1d = 0, nb2d = 0;
+    var nb1d = 0, nb2d = 0, nb1h = 0, nb13c = 0;
     if(nmr) {
         var has1d = nmr.some(function(nmr) { return nmr.dimension === 1 });
         var has2d = nmr.some(function(nmr) { return nmr.dimension === 2 });
         for(var i=0; i<nmr.length; i++) {
-            if (nmr[i].dimension === 1) nb1d++;
-            else if (nmr[i].dimension === 2) nb2d++;
+            for(var i=0; i<nmr.length; i++) {
+                if (nmr[i].dimension === 1) {
+                    nb1d++;
+                    if (Array.isArray(nmr[i].nucleus) && typeof nmr[i].nucleus[0] === 'string') {
+                        if (nmr[i].nucleus[0].toLowerCase() === '1h') {
+                            nb1h++;
+                        } else if (nmr[i].nucleus[0].toLowerCase() === '13c') {
+                            nb13c++;
+                        }
+                    }
+                } else if (nmr[i].dimension === 2) nb2d++;
+            }
         }
     }
 
@@ -32,6 +42,8 @@ exports.getToc = function(doc) {
         has2d: has2d,
         nb1d: nb1d,
         nb2d: nb2d,
+        nb1h: nb1h,
+        nb13c: nb13c,
         modificationDate: doc.$modificationDate
     };
 };
