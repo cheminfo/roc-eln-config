1475df179a193a5e32c450677f4deb8ae9454857
Daniel Kostro <kostro.d@gmail.com>
2026-06-26 14:53:46 +0200

    chore: prettier

--- getReference.js
+++ getReference.js
@@ -1,7 +1,7 @@
 exports.getReference = function (entry) {
-  if(typeof entry.$id === 'string') {
+  if (typeof entry.$id === 'string') {
     return entry.$id;
-  } else if(Array.isArray(entry.$id)) {
+  } else if (Array.isArray(entry.$id)) {
     return entry.$id.join(' ');
   }
   return entry.$id;
